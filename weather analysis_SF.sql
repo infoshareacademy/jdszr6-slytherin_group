4cd26ad93a7c05ecbb176c122e04b4e0fec71590
Joanna Borowa <79273151+Joanna-bot@users.noreply.github.com>
2022-01-02 11:58:05 +0100

    Add files via upload

--- weather analysis_SF.sql
+++ weather analysis_SF.sql
@@ -5,11 +5,11 @@ from weather
 
 /*ekstrakcja miesiaca z daty*/
 
-select substring(date,1,1) as miesiac
+select substring(date,1,2) as miesiac
 from weather
 
 /*polaczenie roku i miesiaca*/
-select substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac
+select substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac
 from weather
 
 
@@ -17,10 +17,10 @@ from weather
 /*srednia maxymalna temperatura na miesiac */
 
 create view v_max_srtemp1 as
-select max_temperature_f, substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select max_temperature_f, substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(max_temperature_f) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_max_temp_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_max_temp_na_miesiac
 from weather
 where zip_code like '94%';
 
@@ -32,10 +32,10 @@ from v_max_srtemp1
 
 /*srednia minimalna temperatura na miesiac (pomijajac lata)*/
 create view v_min_srtemp1 as
-select min_temperature_f, substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select min_temperature_f, substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(min_temperature_f) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_min_temp_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_min_temp_na_miesiac
 from weather
 where zip_code like '94%';
 
@@ -46,10 +46,10 @@ from v_min_srtemp1
 /*srednia temperatura na miesiac liczac wg sredniej*/
 
 create view v_mean_srtemp1 as
-select mean_temperature_f , substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select mean_temperature_f , substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(mean_temperature_f) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_mean_temp_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_mean_temp_na_miesiac
 from weather
 where zip_code like '94%';
 
@@ -59,12 +59,12 @@ from v_mean_srtemp1
 
 /*podzial na  zbiory*/
 
-select zip_code, substring(date,1,1) || '-' || substring(date,-4,4), avg(mean_temperature_f) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)), 
+select zip_code, substring(date,1,2) || '-' || substring(date,-4,4), avg(mean_temperature_f) over 
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)), 
 	ntile(4) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4))
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4))
 from weather
 where zip_code like '94%';
 
@@ -98,10 +98,10 @@ where ranking between 16 AND 20
 
 /*visibility*/
 create view v_meanvisib as
-select mean_visibility_miles , substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select mean_visibility_miles , substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(mean_visibility_miles) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_mean_visibility_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_mean_visibility_na_miesiac
 from weather
 where zip_code like '94%';
 
@@ -134,17 +134,17 @@ from v_meanvisib2
 where ranking between 16 AND 20
 
 
-select max_visibility_miles _temperature_f , substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select max_visibility_miles _temperature_f , substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(max_visibility_miles) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_max_visibility_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_max_visibility_na_miesiac
 from weather
 where zip_code like '94%';
 
-select min_visibility_miles , substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select min_visibility_miles , substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(min_visibility_miles) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_min_visibility_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_min_visibility_na_miesiac
 from weather
 where zip_code like '94%';
 
@@ -153,36 +153,36 @@ where zip_code like '94%';
 
 /*czestotliwosc opadow*/
 
-select count(1), substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac
+select count(1), substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac
 from weather w 
 WHERE precipitation_inches>0 and zip_code like '94%'
-group by substring(date,1,1) || '-' || substring(date,-4,4) 
+group by substring(date,1,2) || '-' || substring(date,-4,4) 
 
 
-select count(1), substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac
+select count(1), substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac
 from weather w 
 WHERE events = 'Rain' and zip_code like '94%'
-group by substring(date,1,1) || '-' || substring(date,-4,4) 
+group by substring(date,1,2) || '-' || substring(date,-4,4) 
 
 /*czestotliwosc mlgy*/
 
-select count(1), substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac
+select count(1), substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac
 from weather w 
 WHERE events = 'Fog' and zip_code like '94%'
-group by substring(date,1,1) || '-' || substring(date,-4,4) 
+group by substring(date,1,2) || '-' || substring(date,-4,4) 
 
 /*czestotliwosc mgly i deszczu*/
 
-select count(1), substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac
+select count(1), substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac
 from weather w 
 WHERE events = 'Fog-Rain' and zip_code like '94%';
-group by substring(date,1,1) || '-' || substring(date,-4,4) 
+group by substring(date,1,2) || '-' || substring(date,-4,4) 
 
 /*kod kontrolny*/
 
-select precipitation_inches, substring(date,1,1) || '-' || substring(date,-4,4) 
+select precipitation_inches, substring(date,1,2) || '-' || substring(date,-4,4) 
 from weather w 
-where substring(date,1,1) || '-' || substring(date,-4,4) = '1-2014' and precipitation_inches >0 and zip_code like '94%'
+where substring(date,1,2) || '-' || substring(date,-4,4) = '1/-2014' and precipitation_inches >0 and zip_code like '94%'
 order by date; 
 
 
@@ -191,19 +191,19 @@ order by date;
 
 /*max opadow*/
 
-select precipitation_inches, substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select precipitation_inches, substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 max(precipitation_inches) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as max_opadow
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as max_opadow
 from weather
 where zip_code like '94%';
 
 /*srednia opadow*/
 create view v_sropadow as
-select precipitation_inches, substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select precipitation_inches, substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(precipitation_inches) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_opadow_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_opadow_na_miesiac
 from weather
 where zip_code like '94%';
 
@@ -243,10 +243,10 @@ where ranking between 16 AND 20
 
 /*sredni wiatr*/ 
 create view v_srwiatr as
-select mean_wind_speed_mph , substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select mean_wind_speed_mph , substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(mean_wind_speed_mph) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_predkosc_wiatru_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_predkosc_wiatru_na_miesiac
 from weather
 where zip_code like '94%';
 
@@ -287,10 +287,10 @@ where ranking between 16 AND 20
 
 /*sredni max wiatr*/
 
-select max_wind_Speed_mph, substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select max_wind_Speed_mph, substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(max_wind_Speed_mph) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_max_predkosc_wiatru_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_max_predkosc_wiatru_na_miesiac
 from weather
 where zip_code like '94%';
 
@@ -299,10 +299,10 @@ where zip_code like '94%';
 /*sredni max poryw wiatru*/
 
 create view v_srmaxporyw as 
-select max_gust_speed_mph, substring(date,1,1) || '-' || substring(date,-4,4) as rok_i_miesiac,  
+select max_gust_speed_mph, substring(date,1,2) || '-' || substring(date,-4,4) as rok_i_miesiac,  
 avg(max_gust_speed_mph) over 
-	(partition by substring(date,1,1) || '-' || substring(date,-4,4) 
-	order by substring(date,1,1) || '-' || substring(date,-4,4)) as sr_max_poryw_wiatru_na_miesiac
+	(partition by substring(date,1,2) || '-' || substring(date,-4,4) 
+	order by substring(date,1,2) || '-' || substring(date,-4,4)) as sr_max_poryw_wiatru_na_miesiac
 from weather
 where zip_code like '94%';
 
