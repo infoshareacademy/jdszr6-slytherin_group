fc827602ad211b6d151cd6d93d961f32d212d5ea
Joanna Borowa <79273151+Joanna-bot@users.noreply.github.com>
2022-01-28 15:57:52 +0100

    JB code-bikes

--- master_code_project.sql
+++ master_code_project.sql
@@ -367,7 +367,7 @@ order by trip_zip_code, ntiles
 
 
 -------------MR-----------------------
---widok tripu z rokmiesi¹c
+--widok tripu z rokmiesiÂ¹c
 create or replace view v_trip_yearmonth as
 select *,
 to_char (tc.start_date, 'yyyy-mm') as year_month
@@ -375,7 +375,7 @@ from v3_trips_with_cities_zip_codes tc
 
 drop view v_trip_yearmonth
 
---po³¹czenie tras_ID
+--poÂ³Â¹czenie tras_ID
 create or replace view v_trip as 
 select * ,
 start_station_id ||'_'|| end_station_id as trip_ID,
@@ -421,12 +421,12 @@ select *
 from v_ranking 
 where ranking <=10
 
---wgrany z csv s³ownik na top10
+--wgrany z csv sÂ³ownik na top10
 select *
 from top10dic tdc 
 
---dogranie s³ownika top 10 do trips by wyliczyæ statystyki
---mo¿na z niej wyci¹gaæ wszystkie dane z tabeli trips po top 10
+--dogranie sÂ³ownika top 10 do trips by wyliczyÃ¦ statystyki
+--moÂ¿na z niej wyciÂ¹gaÃ¦ wszystkie dane z tabeli trips po top 10
 select distinct 
 td.trip_id_a ,
 vt.year_month ,
@@ -471,7 +471,7 @@ join top10dic td
 on td.trip_id = vt.trip_id 
 
 
---fianlana tabela z po³¹czeniem s³ownika, latitude, long, trip qty, ranking
+--fianlana tabela z poÂ³Â¹czeniem sÂ³ownika, latitude, long, trip qty, ranking
 select distinct
 td.trip_id_a ,
 vt.year_month ,
@@ -493,3 +493,150 @@ on sc.id::text = left(td.trip_id_a,2)
 join station_csv sc2 
 on sc2.id::text = substring(td.trip_id_a from 4 for 2)
 
+
+--------------------------JB--------------------------
+
+
+/*weekendy*/
+create view v_weekendy as
+SELECT distinct time, extract(year from time)||'-'||extract(month from time)||'-'||extract(day from time) as nowa_data
+  FROM status_csv sc 
+ WHERE EXTRACT(ISODOW FROM sc.time) IN (6, 7)
+ 
+ /*dni pracujace*/
+create view v_dni_robocze1 as
+SELECT distinct time, extract(year from time)||'-'||extract(month from time)||'-'||extract(day from time) as nowa_data
+  FROM status_csv sc 
+ WHERE EXTRACT(ISODOW FROM sc.time) IN (1, 2,3,4,5)
+ 
+ /*ekstrakcja godziny *//
+ select extract(hour from time) as godzina
+ from status_csv sc 
+
+ /*podzial na pory dnia*/
+create view v_pora_dnia1 as
+SELECT time, CASE 
+ 	  WHEN h <= 6  or h > 22  THEN 'night'  -- including 0 !
+    WHEN h >  6  AND h <= 12 THEN 'morning'
+    WHEN h > 12  AND h <= 22 THEN 'evening'
+       END AS moment
+FROM  (
+    SELECT time, extract(hour from time) AS h
+    FROM   status_csv sc 
+    ) x
+
+
+
+select distinct station_id, bikes_available, 
+extract(year from time)||'-'||extract(month from time)||'-'||extract(day from time) as nowa_data, 
+case
+  when EXTRACT(ISODOW FROM sc.time) IN (1, 2,3,4,5) then 'working_days'
+  when EXTRACT(ISODOW FROM sc.time) IN (6, 7) then 'weekends'
+  end as part_of_the_week,
+from status_csv
+join v_pora_dnia1 
+on v_pora_dnia1.time=sc.time
+
+
+
+/*srednia, min i max rowerow po stacji i dniu*/
+select 
+sc2.station_id, 
+avg(sc2.bikes_available),
+min(sc2.bikes_available),max(sc2.bikes_available),
+extract(year from time)||'-'||extract(month from time)||'-'||extract(day from time) as nowa_data
+from status_csv sc2 
+group by sc2.station_id, extract(year from time)||'-'||extract(month from time)||'-'||extract(day from time)
+
+/*srednia, min i max rowerow po stacji i w weekendy*/
+select *,
+avg(sc.bikes_available) over (partition by nowa_data) as sr_w_weekend,
+min(sc.bikes_available) over (partition by nowa_data) as min_w_weekend,
+max(sc.bikes_available) over (partition by nowa_data) as max_w_weekend
+from status_csv sc 
+join v_weekendy 
+on v_weekendy.time=sc.time
+
+
+/*srednia, min i max rowerow po stacji i w dni pracujace*/
+
+select *,
+avg(sc.bikes_available) over (partition by  nowa_data) as sr_w_drobocze,
+min(sc.bikes_available) over (partition by  nowa_data) as min_w_drobocze,
+max(sc.bikes_available) over (partition by nowa_data) as max_w_d_robocze
+from status_csv sc 
+join v_dni_robocze1
+on v_dni_robocze1.time=sc.time
+
+
+
+/*polaczenie z tripami Mateusza */
+--na miesiace
+select distinct v_top10a.trip_id,
+extract(year from sc2.time)||'-'||extract(month from sc2.time) as rok_miesiac,
+avg(sc2.bikes_available) over (partition by  extract(year from sc2.time)||'-'||extract(month from sc2.time)) as sr_miesieczna,
+min(sc2.bikes_available) over (partition by  extract(year from sc2.time)||'-'||extract(month from sc2.time)) as min_miesieczne,
+max(sc2.bikes_available) over (partition by  extract(year from sc2.time)||'-'||extract(month from sc2.time)) as max_miesieczne
+from status_csv sc2 
+join v_top10a 
+on sc2.station_id =v_top10a.start_station_id
+
+-- srednia pokazuje, ze jeszcze jest potencjal na te trasy we wszystkich miesiacach, okolo 50% rowerow jest jeszcze dostepnych 
+
+--na weekendy
+select distinct v_top10a.trip_id,nowa_data,
+avg(sc.bikes_available) over (partition by nowa_data) as sr_w_weekend,
+min(sc.bikes_available) over (partition by nowa_data) as min_w_weekend,
+max(sc.bikes_available) over (partition by nowa_data) as max_w_weekend
+from status_csv sc 
+join v_weekendy 
+on v_weekendy.time=sc.time
+join v_top10a 
+on sc.station_id =v_top10a.start_station_id
+
+
+--na dni robocze
+select distinct *,
+avg(sc.bikes_available) over (partition by  nowa_data) as sr_w_drobocze,
+min(sc.bikes_available) over (partition by  nowa_data) as min_w_drobocze,
+max(sc.bikes_available) over (partition by nowa_data) as max_w_drobocze
+from status_csv sc 
+join v_dni_robocze
+on v_dni_robocze.time=sc.time
+join v_top10a 
+on sc.station_id =v_top10a.start_station_id
+
+--na pory dnia
+select distinct v_top10a.trip_id, moment,
+avg(sc2.bikes_available) over (partition by  moment ) as sr_na_pore_dnia,
+min(sc2.bikes_available) over (partition by  moment) as min_na_pore_dnia,
+max(sc2.bikes_available) over (partition by  moment) as max_na_pore_dnia
+from status_csv sc2 
+join v_top10a 
+on sc.station_id =v_top10a.start_station_id
+join v_pora_dnia1
+on v_pora_dnia1.station_id=sc.station_id
+
+--
+
+
+/*z ktorych stacji do ktorych i kiedy warto przerzucic rowery*/
+
+
+create view v_przen_rower as
+select distinct *,
+case
+	 when srednia >=10 then 'przeniesc rower'
+	 when srednia between 7 and 9 then 'zostawic'
+	 when srednia <=6 then 'tu przeniesc'
+	 else 'zostawic' end czy_przeniesc_rower
+from
+	(select sc.station_id, avg(sc.bikes_available) over 
+	(partition by extract(year from sc.time)||'-'||extract(month from sc.time)) as srednia
+	from status_csv sc 
+	group by sc.station_id, extract(year from sc.time)||'-'||extract(month from sc.time), sc.bikes_available )y 
+
+select *
+from v_przen_rower
+join v_ranking2 
+on v_ranking2.start_station_id=v_przen_rower.station_id
